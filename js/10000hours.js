c00a410cce860c3af21a7dd47953873d5b1eadc5
baeggoose <bsj154@gmail.com>
2023-04-29 14:59:02 +0900

    JavaScript querySelector

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,3 +1,11 @@
+const startButton = document.querySelector(".start_btn");
+const openButton = document.querySelector(".modal_btn");
+const closeButton = document.querySelector(".close_btn");
+const shareButton = document.querySelector(".share_btn");
+const result = document.querySelector(".result");
+const modal = document.querySelector("#modal");
+const loading = document.querySelector(".result_loading");
+
 shareButton.addEventListener("click", copyUrl);
 openButton.addEventListener("click", openModal);
 closemodal.addEventListener("click", closeModal);
