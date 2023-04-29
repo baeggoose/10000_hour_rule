5623eaec2ece241ef435b85a57387d9ee439cb17
baeggoose <bsj154@gmail.com>
2023-04-29 14:51:12 +0900

    JavaScript addEventListener

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,1 +1,4 @@
-// js file
+shareButton.addEventListener("click", copyUrl);
+openButton.addEventListener("click", openModal);
+closemodal.addEventListener("click", closeModal);
+startButton.addEventListener("click", calculator);
